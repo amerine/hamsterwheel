1ee0ece393eb44f14bdbbdcf321f5eb97cdc83d2
Mark Turner <mark@amerine.net>
2017-11-22 23:16:35 -0800

    server: Get migrations running

--- Makefile
+++ Makefile
@@ -1,9 +1,14 @@
-.PHONY: test
+.PHONY: test install migrations migrate migrations hamsterwheel-migrations
 
-install:
+hamsterwheel-migrations:
+	go-bindata -pkg database -o cmd/hamsterwheel-server/internal/database/migrations.go -prefix "cmd/hamsterwheel-server/db/migrations/" cmd/hamsterwheel-server/db/migrations/...
+
+migrations: hamsterwheel-migrations
+
+install: migrations
 	go install ./cmd/hamsterwheel-server
 
-test: install
+test: migrations install
 	dropdb hamsterwheel-test; createdb hamsterwheel-test
 	go test -v ./...
 	forego run -e .env.test hamsterwheel-server
--- cmd/hamsterwheel-server/main.go
+++ cmd/hamsterwheel-server/main.go
@@ -6,6 +6,9 @@ import (
 	"github.com/amerine/hamsterwheel/cmd/hamsterwheel-server/internal/database"
 	"github.com/joeshaw/envdecode"
 	_ "github.com/lib/pq"
+	"github.com/mattes/migrate"
+	"github.com/mattes/migrate/database/postgres"
+	bindata "github.com/mattes/migrate/source/go-bindata"
 )
 
 func main() {
@@ -15,8 +18,35 @@ func main() {
 		log.Fatal(err)
 	}
 
-	_, err := database.New(cfg.DatabaseURL)
+	db, err := database.New(cfg.DatabaseURL)
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	migrDBDriver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	migrAssets := bindata.Resource(database.AssetNames(),
+		func(name string) ([]byte, error) {
+			return database.Asset(name)
+		})
+
+	migrSrcDriver, err := bindata.WithInstance(migrAssets)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithInstance("<go-bindata>", migrSrcDriver, "doesnt-matter", migrDBDriver)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Start: Running Migrations")
+	err = m.Up()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("Done: Running Migrations")
 }
